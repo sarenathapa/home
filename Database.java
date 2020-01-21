7e9962e66a88adcea898e687b27446e6d6da4c89
sarena thapa <44331505+sarenathapa@users.noreply.github.com>
2020-01-21 10:49:24 +0530

    Update Database.java

--- Database.java
+++ Database.java
@@ -70,16 +70,7 @@ public class Database {
 							}
 					}
 				
-					/*if(null !=row.getCell(12)) {
-						ps.setString(7, row.getCell(12).toString());
-					}*/
-				/*	String sql1 = "select ProjectId from comp where Competency='"+row.getCell(12).toString()+"'";
-					ResultSet rs1=stat.executeQuery(sql1); 
-					if(null != row.getCell(4)) {
-						if(rs1.next()) {
-                   
-						}
-					}*/
+
 				try {
 					ps.executeUpdate();
 				//	System.out.println(myset);
@@ -97,59 +88,5 @@ public class Database {
 }
 			
 }	
-					//ps.setString(7,row.getCell(12).toString());
-					//ps.setString(8,row.getCell(12).toString());
-			//	System.out.println("Values Inserted Successfully");
-					/*if (null != sheet.getRow(i).getCell(0) && !sheet.getRow(i).getCell(0).toString().equals("")) {
-						myset.add(sheet.getRow(i).getCell(0).toString());
-						System.out.println(sheet.getRow(i).getCell(0));
-						ps.executeUpdate();
-					}
-					if (null != sheet.getRow(i).getCell(2) && !sheet.getRow(i).getCell(2).toString().equals("")) {
-						myset.add(sheet.getRow(i).getCell(2).toString());
-						System.out.println(sheet.getRow(i).getCell(2));
-						ps.executeUpdate();
-					}
-					if (null != sheet.getRow(i).getCell(3) && !sheet.getRow(i).getCell(3).toString().equals("")) {
-						myset.add(sheet.getRow(i).getCell(3).toString());
-						System.out.println(sheet.getRow(i).getCell(3));
-						ps.executeUpdate();
-					}
-					if (null != sheet.getRow(i).getCell(5) && !sheet.getRow(i).getCell(5).toString().equals("")) {
-						myset.add(sheet.getRow(i).getCell(5).toString());
-						System.out.println(sheet.getRow(i).getCell(5));
-						ps.executeUpdate();
-					}
-					if (null != sheet.getRow(i).getCell(6) && !sheet.getRow(i).getCell(6).toString().equals("")) {
-						myset.add(sheet.getRow(i).getCell(6).toString());
-						System.out.println(sheet.getRow(i).getCell(6));
-						ps.executeUpdate();
-					}
-					if (null != sheet.getRow(i).getCell(10) && !sheet.getRow(i).getCell(10).toString().equals("")) {
-						myset.add(sheet.getRow(i).getCell(10).toString());
-						System.out.println(sheet.getRow(i).getCell(10));
-						ps.executeUpdate();
-					}
-					if (null != sheet.getRow(i).getCell(12) && !sheet.getRow(i).getCell(12).toString().equals("")) {
-						myset.add(sheet.getRow(i).getCell(12).toString());
-						System.out.println(sheet.getRow(i).getCell(12));
-						ps.executeUpdate();
-					}
-					//ps.executeUpdate();
-				}
-			}*/
-		//	System.out.println(myset);
-		//	System.out.println(ps.executeUpdate());
-			// System.out.println(sheet.getRow(i).getCell(5));
-			// Iterator<String> itr = myset.iterator();
-			// int i=1;
-			/*
-			 * int i=1; while(itr.hasNext()) { ps.getString(1,itr.next());
-			 * ps.getString(2,itr.next()); ps.getString(3,itr.next());
-			 * ps.getString(5,itr.next()); ps.getString(6,itr.next());
-			 * ps.getString(10,itr.next()); ps.getString(12,itr.next()); i++;
-			 * 
-			 * ps.executeUpdate(); }
-			 */
-		
+					
 
